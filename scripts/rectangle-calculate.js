972f044779c49bc3583a3422e26162d7b50ae441
mahade <mahade.adib45@gmail.com>
2024-02-13 20:07:41 +0600

    edited

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,18 @@
 function calculateRectangleArea(){
-    
+    //get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length')
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+
+    //get width of the rectangle
+    const widthInput = document.getElementById( 'rectangle-width' );
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+
+    //calculate rectangle area
+    const area = length * width;
+
+    //display rectangle area
+    const RectangleAreaSpan = document.getElementById('rectangle-area');
+    RectangleAreaSpan.innerText = area;
 }
